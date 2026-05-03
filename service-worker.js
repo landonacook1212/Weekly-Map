2886bf5c25fea17bf6969afd1f708b332f18e385
landonacook1212 <landon.a.cook1212@gmail.com>
2026-05-03 07:55:25 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'weekly-map-dashboard-timeline-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => {
+      if (key !== CACHE_NAME) return caches.delete(key);
+    })))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
