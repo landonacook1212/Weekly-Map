15b9484c5b3001d89a79493721a8c632772e1619
landonacook1212 <landon.a.cook1212@gmail.com>
2026-05-03 09:32:10 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,8 @@
-const CACHE_NAME = 'weekly-map-manual-energy-guide-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
+const CACHE_NAME = 'weekly-map-four-branches-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => {
-      if (key !== CACHE_NAME) return caches.delete(key);
-    })))
-  );
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
